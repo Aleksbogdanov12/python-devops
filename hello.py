06c4981ae9bdd48b172e8ac0095ba5b6b86e0756
Aleksbogdanov12 <119742953+Aleksbogdanov12@users.noreply.github.com>
2023-02-14 11:15:07 +0000

    adding command tool

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 from devopslib.randomfruit import fruit
+import fire
 
-print(fruit())
+def meal(beverage):
+    my_fruit = fruit()
+    print(f"My {my_fruit} and {beverage}")
+    if my_fruit == "cherry":
+        complete_meal = f"Your meal is my fruit {my_fruit} and {beverage}"
+        return complete_meal
+    alternate_meal = f"Your meal is a steak and {beverage}"
+    return alternate_meal
+
+if __name__ == "__main__":
+    fire.Fire(meal)
